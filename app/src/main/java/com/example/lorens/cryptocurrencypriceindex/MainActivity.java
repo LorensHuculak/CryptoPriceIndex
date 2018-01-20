f80ce7eb368cf17aecc0d3286c85ca7941f3c9fe
LorensHuculak <huculaklorens@yahoo.com>
2018-01-20 17:29:55 +0100

    Basic Functionality

---
+++ app/src/main/java/com/example/lorens/cryptocurrencypriceindex/MainActivity.java
@@ -0,0 +1,253 @@
+package com.example.lorens.cryptocurrencypriceindex;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.Menu;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String BPI_ENDPOINT = "https://api.coindesk.com/v1/bpi/currentprice.json";
+    public static final String ETH_ENDPOINT = "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=EUR,USD,GBP";
+    public static final String NEO_ENDPOINT = "https://min-api.cryptocompare.com/data/price?fsym=NEO&tsyms=EUR,USD,GBP";
+    private OkHttpClient okHttpClient = new OkHttpClient();
+    private ProgressDialog progressDialog;
+
+    private TextView txt;
+    private ImageView img;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_bitcoin:
+                    loadBTC();
+                    return true;
+                case R.id.navigation_ethereum:
+                    loadETH();
+                    return true;
+                case R.id.navigation_neo:
+                    loadNEO();
+                    return true;
+            }
+            return false;
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txt = (TextView) findViewById(R.id.txt);
+        img = (ImageView) findViewById(R.id.logo);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("BPI Loading");
+        progressDialog.setMessage("Wait ...");
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.login) {
+            txt.setText("Extra Feature");
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void loadBTC() {
+        Request request = new Request.Builder()
+                .url(BPI_ENDPOINT)
+                .build();
+
+        progressDialog.show();
+
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Toast.makeText(MainActivity.this, "Error during BPI loading : "
+                        + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response)
+                    throws IOException {
+                final String body = response.body().string();
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressDialog.dismiss();
+                        parseBpiResponse(body);
+                    }
+                });
+            }
+        });
+
+    }
+
+    private void parseBpiResponse(String body) {
+        try {
+            StringBuilder builder = new StringBuilder();
+
+            JSONObject jsonObject = new JSONObject(body);
+          /**  JSONObject timeObject = jsonObject.getJSONObject("time");
+            builder.append(timeObject.getString("updated")).append("\n\n"); */
+
+            JSONObject bpiObject = jsonObject.getJSONObject("bpi");
+
+            JSONObject euroObject = bpiObject.getJSONObject("EUR");
+            builder.append("€ ").append(euroObject.getString("rate")).append("\n");
+
+            JSONObject usdObject = bpiObject.getJSONObject("USD");
+            builder.append("$ ").append(usdObject.getString("rate")).append("\n");
+
+            JSONObject gbpObject = bpiObject.getJSONObject("GBP");
+            builder.append("£ ").append(gbpObject.getString("rate")).append("\n");
+
+
+
+            txt.setText(builder.toString());
+            img.setImageResource(R.drawable.bitcoin);
+
+        } catch (Exception e) {
+
+        }
+    }
+
+    private void loadETH() {
+        Request request = new Request.Builder()
+                .url(ETH_ENDPOINT)
+                .build();
+
+        progressDialog.show();
+
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Toast.makeText(MainActivity.this, "Error during ETH loading : "
+                        + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response)
+                    throws IOException {
+                final String body = response.body().string();
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressDialog.dismiss();
+                        parseEthResponse(body);
+                    }
+                });
+            }
+        });
+
+    }
+
+    private void parseEthResponse(String body) {
+        try {
+            StringBuilder builder = new StringBuilder();
+
+            JSONObject jsonObject = new JSONObject(body);
+
+
+            builder.append("€ ").append(jsonObject.getString("EUR")).append("\n");
+
+            builder.append("$ ").append(jsonObject.getString("USD")).append("\n");
+
+
+            builder.append("£ ").append(jsonObject.getString("GBP")).append(" ").append("\n");
+
+
+
+            txt.setText(builder.toString());
+            img.setImageResource(R.drawable.ethereum);
+
+        } catch (Exception e) {
+
+        }
+    }
+
+
+    private void loadNEO() {
+        Request request = new Request.Builder()
+                .url(NEO_ENDPOINT)
+                .build();
+
+        progressDialog.show();
+
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Toast.makeText(MainActivity.this, "Error during NEO loading : "
+                        + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response)
+                    throws IOException {
+                final String body = response.body().string();
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressDialog.dismiss();
+                        parseNeoResponse(body);
+                    }
+                });
+            }
+        });
+
+    }
+
+    private void parseNeoResponse(String body) {
+        try {
+            StringBuilder builder = new StringBuilder();
+
+            JSONObject jsonObject = new JSONObject(body);
+
+            builder.append("€ ").append(jsonObject.getString("EUR")).append("\n");
+
+            builder.append("$ ").append(jsonObject.getString("USD")).append("\n");
+
+
+            builder.append("£ ").append(jsonObject.getString("GBP")).append(" ").append("\n");
+
+
+            txt.setText(builder.toString());
+            img.setImageResource(R.drawable.neo);
+
+        } catch (Exception e) {
+
+        }
+    }
+}
